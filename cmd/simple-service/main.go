176bb0ade9a9d50795c4eb3359d3826ea761851d
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-02-18 12:33:10 +0600

    added time + reading from console

--- cmd/simple-service/main.go
+++ cmd/simple-service/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("ggg")
+	var name string
+	var price int             //сами высчитываем
+	var income_outcome string //возмодно bool, расход либо доход
+	var comment string
+	var category string
+	date := time.Now()
+
+	fmt.Fscan(os.Stdin, &name)
+	fmt.Fscan(os.Stdin, &price)
+	fmt.Fscan(os.Stdin, &income_outcome)
+	fmt.Fscan(os.Stdin, &comment)
+	fmt.Fscan(os.Stdin, &category)
+	fmt.Println(date.Day(), date.Month(), date.Year())
 }
